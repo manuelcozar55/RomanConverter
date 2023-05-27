7bdaab057cacc4f022b3071b11af0766bc14baee
manuelcozar55 <125476283+manuelcozar55@users.noreply.github.com>
2023-05-27 20:21:43 +0200

    Update romans.js

--- romans.js
+++ romans.js
@@ -35,10 +35,7 @@ function init() {
     } else {
       alert(convertion.message);
     }
-    // eslint-disable-next-line no-undef
-    gtag('event', 'conversion_done', {
-      'conversionType': conversionType
-    });
+    
   }); 
 }
 
